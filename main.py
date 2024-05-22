4273d64643d77225a20e5f1c776040f808fa5103
binqbit <binqbit@gmail.com>
2024-05-23 00:10:32 +0300

    added creation of many temp files

--- main.py
+++ main.py
@@ -6,23 +6,27 @@ import requests
 from zipfile import ZipFile
 
 if len(sys.argv) > 1:
-    file_name = sys.argv[1]
-    if file_name.endswith('/') or file_name.endswith('\\'):
-        file_name = file_name[:-1]
-    is_dir = os.path.isdir(file_name)
-    if is_dir:
-        with ZipFile(file_name + '.zip', 'w') as zip:
-            for root, dirs, files in os.walk(file_name):
-                for file in files:
-                    zip.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(file_name, '..')))
-        file_name = file_name + '.zip'
+    file_name = 'temp_files.zip'
+    files = sys.argv[1:]
+
+    for i in range(len(files)):
+        if files[i].endswith('/') or files[i].endswith('\\'):
+            files[i] = files[i][:-1]
+            
+    with ZipFile(file_name, 'w') as zip:
+        for file in files:
+            if os.path.isdir(file):
+                for root, dirs, files in os.walk(file):
+                    for f in files:
+                        zip.write(os.path.join(root, f), os.path.relpath(os.path.join(root, f), os.path.join(file, '..')))
+            else:
+                zip.write(file, os.path.basename(file))
         
     response = requests.request('POST', 'https://tmpfiles.org/api/v1/upload', files={'file': open(file_name, 'rb')})
     os.remove(file_name)
+    
     url = response.json()['data']['url'].replace('https://tmpfiles.org/', 'https://tmpfiles.org/dl/')
-    cmd = f'curl -o {file_name} {url}'
-    if is_dir:
-        cmd = cmd + ' && unzip ' + file_name + ' && rm ' + file_name
+    cmd = f'curl -o {file_name} {url}' + ' && unzip ' + file_name + ' && rm ' + file_name
     print(cmd)
     pyperclip.copy(cmd)
 else:
